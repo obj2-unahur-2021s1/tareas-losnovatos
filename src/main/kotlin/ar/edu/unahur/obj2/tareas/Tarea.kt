3699e17cfe0a94e39ae0831fdd024cb44c3a9e83
JulianDiani <juliandiani95@gmail.com>
2021-06-05 20:21:33 -0300

    implementada la clase tarea

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,12 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+abstract class Tarea(val horasEstimadasDeFinalizacion:Double,val responsable:Empleado,val empleadosAsignados: List<Empleado>?,val costoInfraestructura:Double){//la lista de empleados la hago anulable para probar despues heredar sin cargarle nada
+    abstract fun horasNecesariasParaFinalizacion():Double
+    fun costo()=costoInfraestructura+sueldoEmpleados()+sueldoResponsable()
+    abstract fun sueldoEmpleados():Double
+    abstract fun sueldoResponsable():Double
+
+
+
+}
+class Empleado(val sueldoPorHora:Int)
