a309c25d7c7d14c93649966745e49688131b42aa
JulianDiani <juliandiani95@gmail.com>
2021-06-05 21:18:52 -0300

    implementada a medias tareaDeIntegracion

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -6,8 +6,14 @@ interface Tarea {
 }
 class TareaSimple(val horasEstimadasParaFinalizacion:Double,val responsable:Empleado,val empleadosAsignados:List<Empleado>,val costoInfraestructura:Double): Tarea {
     override fun horasNecesariasParaFinalizacion()=this.horasEstimadasParaFinalizacion/this.empleadosAsignados.size
-    override fun costoDeTarea()=this.costoInfraestructura+(sueldoEmpleados()*horasNecesariasParaFinalizacion())
-    fun sueldoEmpleados()=this.empleadosAsignados.sumBy { it.sueldoPorHora }*this.horasNecesariasParaFinalizacion()
+    override fun costoDeTarea()=this.costoInfraestructura+ sueldoTotalEmpleados()+sueldoResponsable()
+    fun sueldoTotalEmpleados()=this.empleadosAsignados.sumBy { it.sueldoPorHora }*this.horasNecesariasParaFinalizacion()
     fun sueldoResponsable()=horasEstimadasParaFinalizacion
 }
+class TareaDeIntegracion(val responsable: Empleado):Tarea{
+    override fun horasNecesariasParaFinalizacion()=
+    override fun costoDeTarea()=
+}
+
+
 class Empleado(val sueldoPorHora:Int)
