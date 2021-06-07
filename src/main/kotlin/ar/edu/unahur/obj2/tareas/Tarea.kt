09a66169c8cc385c4a40c4e8fd13cded65322f52
leozeballez <zeballezleonel@hotmail.com>
2021-06-07 02:21:20 -0300

    Implementados requerimientos. Agregados los tests de Tareas Simples. Faltan tests que prueban funcionalidad de Tareas De Integracion.

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,19 +1,27 @@
 package ar.edu.unahur.obj2.tareas
 
-interface Tarea {
-    fun horasNecesariasParaFinalizacion() : Double
-    fun costoDeTarea() : Double
+abstract class Tarea(val responsable: Empleado) {
+    abstract fun horasNecesariasParaFinalizacion() : Int
+    abstract fun costoDeTarea() : Double
+    abstract fun nominaDeEmpleados(): List<Empleado>
 }
-class TareaSimple(val horasEstimadasParaFinalizacion:Double,val responsable:Empleado,val empleadosAsignados:List<Empleado>,val costoInfraestructura:Double): Tarea {
-    override fun horasNecesariasParaFinalizacion()=this.horasEstimadasParaFinalizacion/this.empleadosAsignados.size
-    override fun costoDeTarea()=this.costoInfraestructura+ sueldoTotalEmpleados()+sueldoResponsable()
-    fun sueldoTotalEmpleados()=this.empleadosAsignados.sumBy { it.sueldoPorHora }*this.horasNecesariasParaFinalizacion()
-    fun sueldoResponsable()=horasEstimadasParaFinalizacion
-}
-class TareaDeIntegracion(val responsable: Empleado):Tarea{
-    override fun horasNecesariasParaFinalizacion()=
-    override fun costoDeTarea()=
+class Simple(val horasEstimadas: Int, responsable:Empleado, val empleadosAsignados:List<Empleado>, val costoInfraestructura:Double): Tarea(responsable) {
+    override fun horasNecesariasParaFinalizacion()=this.horasEstimadas/this.empleadosAsignados.size
+    override fun costoDeTarea()=this.costoInfraestructura + sueldoTotalEmpleados() + sueldoResponsable()
+    override fun nominaDeEmpleados() = empleadosAsignados.plusElement(responsable)
+
+    fun sueldoTotalEmpleados()=this.empleadosAsignados.sumByDouble { this.horasNecesariasParaFinalizacion() * it.sueldoPorHora }
+    fun sueldoResponsable()= horasEstimadas * responsable.sueldoPorHora
 }
 
+class DeIntegracion(responsable: Empleado, val tareas: List<Tarea>):Tarea(responsable) {
+    override fun horasNecesariasParaFinalizacion()= this.horasNecesariasDeSubtareas() + this.horasDeReunionDePlanificacion()
+    fun horasNecesariasDeSubtareas() = tareas.sumBy { it.horasNecesariasParaFinalizacion() }
+    fun horasDeReunionDePlanificacion() = this.horasNecesariasDeSubtareas() / 8
+    override fun costoDeTarea()= this.costosDeSubtareas() + this.bonusDeResponsable()
+    override fun nominaDeEmpleados() = tareas.map { it.nominaDeEmpleados() }.flatten()
+    fun costosDeSubtareas() = tareas.sumByDouble { it.costoDeTarea() }
+    fun bonusDeResponsable() = this.costosDeSubtareas() * 0.03
+}
 
-class Empleado(val sueldoPorHora:Int)
+class Empleado(val sueldoPorHora:Double)
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,29 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
-  describe("Una tarea") {
+
+  describe("Baufest") {
+    val responsable = Empleado(150.0)
+    val empleado1 = Empleado(100.0)
+    val empleado2 = Empleado(80.0)
+    val tareaSimple = Simple(5, responsable, listOf(empleado1, empleado2), 500.0)
+
+    describe("Una tarea") {
+      describe("simple") {
+        it("tiene una nomina de 3 empleados (incluido el responsable") {
+          tareaSimple.nominaDeEmpleados().shouldContainExactlyInAnyOrder(empleado1, empleado2, responsable)
+        }
+        it("sabe cuantas horas se necesitan para finalizar la tarea") {
+          tareaSimple.horasNecesariasParaFinalizacion().shouldBe(2)
+        }
+        it("sabe su costo") {
+          tareaSimple.costoDeTarea().shouldBe(1610)
+        }
+      }
+    }
   }
 })
