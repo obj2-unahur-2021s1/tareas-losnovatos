2b0d32ffb0c621b172edd8179ba205b102e32521
leozeballez <zeballezleonel@hotmail.com>
2021-06-07 02:42:30 -0300

    Agregada una funcion auxiliar: nominaDeEmpleadosDeSubtareas() a la Tarea DeIntegracion.

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -19,7 +19,8 @@ class DeIntegracion(responsable: Empleado, val tareas: List<Tarea>):Tarea(respon
     fun horasNecesariasDeSubtareas() = tareas.sumBy { it.horasNecesariasParaFinalizacion() }
     fun horasDeReunionDePlanificacion() = this.horasNecesariasDeSubtareas() / 8
     override fun costoDeTarea()= this.costosDeSubtareas() + this.bonusDeResponsable()
-    override fun nominaDeEmpleados() = tareas.map { it.nominaDeEmpleados() }.flatten().plusElement(responsable)
+    override fun nominaDeEmpleados() = this.nominaDeEmpleadosDeSubtareas().plusElement(responsable)
+    fun nominaDeEmpleadosDeSubtareas() = tareas.map { it.nominaDeEmpleados() }.flatten()
     fun costosDeSubtareas() = tareas.sumByDouble { it.costoDeTarea() }
     fun bonusDeResponsable() = this.costosDeSubtareas() * 0.03
 }
